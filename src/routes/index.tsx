f18114a79a6f247e406c2f2cf78a6ca36ecf850a
Caleb Taylor <aquaductape@gmail.com>
2024-04-22 22:03:49 -0700

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,4 @@
 import Counter from "~/components/Counter";
-// import TextEditor from "~/components/TextEditor";
-import { clientOnly } from "@solidjs/start";
-const TextEditor = clientOnly(() => import("~/components/TextEditor"));
-
-import { cache, createAsync, redirect } from "@solidjs/router";
-import {
-  Component,
-  Suspense,
-  SuspenseList,
-  createEffect,
-  createResource,
-  createSignal,
-} from "solid-js";
-import RPC from "~/components/RPC";
 
 export default function Home() {
   return (
@@ -21,13 +7,6 @@ export default function Home() {
         Hello world!
       </h1>
       <Counter />
-      <Suspense fallback={<div>waiting 1 seconds...</div>}>
-        <RPC timeout={2000} />
-      </Suspense>
-      {/* <Suspense fallback={<div>waiting 5 seconds...</div>}>
-          <RPC timeout={5000} />
-        </Suspense> */}
-      {/* <TextEditor /> */}
     </main>
   );
 }
